be48da7fca320b3120d095debaa4c87daf911f49
fy <444818604@qq.com>
2020-10-21 17:52:28 +0800

    fu

---
+++ js/footer.js
@@ -0,0 +1,41 @@
+document.writeln(`<div class="footer">
+			<div class="footer-top">
+				<div class="wrap">
+					<div class="footer-left">
+						<img class="footer-logo" src="./img/logo.png" >
+						<div>电话: 4009-000-163</div>
+						<div>邮箱: help@qiyukf.com</div>
+						<div>商务合作: bd@qiyukf.com</div>
+						<div class="flex footer-icon">
+							<img class="mr5" src="img/index17.png" >
+							<img src="img/index16.png" >
+						</div>
+					</div>
+					<ul class="footer-list">
+						<li>
+							<div class="footer-list-title"><a href="#">产品</a></div>
+							<div><a href="#">关于我们</a></div>
+							<div><a href="#">联系我们</a></div>
+							<div><a href="#">公告中心</a></div>
+							<div><a href="#">新闻</a></div>
+						</li>
+						<li>
+							<div class="footer-list-title"><a href="#">信息</a></div>
+							<div><a href="#">关于我们</a></div>
+							<div><a href="#">联系我们</a></div>
+							<div><a href="#">公告中心</a></div>
+							<div><a href="#">新闻</a></div>
+						</li>
+						<li>
+							<div class="footer-list-title"><a href="#">工具链接</a></div>
+							<div><a href="#">Buy Bitcoins</a></div>
+							<div><a href="#">Sell Bitcoins</a></div>
+							<div><a href="#">Exchange</a></div>
+							<div><a href="#">Free Schedule</a></div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="copy">Copyright &copy; 2020 沪ICP备19043558号-1</div>
+		</div>
+`)
---
+++ js/header.js
@@ -0,0 +1,35 @@
+document.writeln(`<div class="header-cont">
+			<div class="header-top">
+				<div class="wrap">
+					<div class="trumpet">
+						<img src="img/index1.png">
+						<div class="trumpet-msg">EHash四周年“礼”：老用户专享电费补贴券</div>
+					</div>
+				</div>
+			</div>
+			<div class="header-btm">
+				<div class="wrap">
+					<div class="flex-between top-line">
+						<div class="flex logo-wrap">
+							<img src="./img/logo.png">
+							<ul class="menu">
+								<li class="active"><a href="#">首页</a></li>
+								<li><a href="#">价格</a></li>
+								<li><a href="#">新闻</a></li>
+								<li><a href="#">帮助中心</a></li>
+								<li><a href="#">我的算力</a></li>
+							</ul>
+						</div>
+						<div class="flex login-op">
+							<div class="login">登陆</div>
+							<div class="regist">注册</div>
+							<div class="lang-op">
+								<div>CN</div>
+								<div class="arrow-btm"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+`)
