b33833e590b7d2fb1be15e31b76145a2675012ea
fy <444818604@qq.com>
2020-12-05 17:44:51 +0800

    fu

--- js/footer.js
+++ js/footer.js
@@ -3,13 +3,8 @@ document.writeln(`<div class="footer">
 				<div class="wrap">
 					<div class="footer-left">
 						<img class="footer-logo" src="./img/logo.png" >
-						<div>电话: 4009-000-163</div>
-						<div>邮箱: help@qiyukf.com</div>
-						<div>商务合作: bd@qiyukf.com</div>
-						<div class="flex footer-icon">
-							<img class="mr5" src="img/index17.png" >
-							<img src="img/index16.png" >
-						</div>
+						<div>邮箱: love@ehash.com, service@ehash.com</div>
+						<div>商务合作: love@ehash.com, service@ehash.com</div>
 					</div>
 					<ul class="footer-list">
 						<li>
@@ -25,13 +20,6 @@ document.writeln(`<div class="footer">
 							<div><a href="#">联系我们</a></div>
 							<div><a href="#">公告中心</a></div>
 							<div><a href="#">新闻</a></div>
-						</li>
-						<li>
-							<div class="footer-list-title"><a href="#">工具链接</a></div>
-							<div><a href="#">Buy Bitcoins</a></div>
-							<div><a href="#">Sell Bitcoins</a></div>
-							<div><a href="#">Exchange</a></div>
-							<div><a href="#">Free Schedule</a></div>
 						</li>
 					</ul>
 				</div>
--- js/header.js
+++ js/header.js
@@ -22,13 +22,13 @@ document.writeln(
 							</ul>
 						</div>
 						<div class="flex login-op">
-							<a v-show="userName1 == ''" href="auth.html?type=1" class="login">登陆</a>
+							<a v-show="userName1 == ''" href="auth.html?type=1" class="login">登录</a>
 							<a v-show="userName1 == ''" href="auth.html?type=2" class="regist">注册</a>
 							<a href="person.html" v-show="userName1 != ''" class="head-user-wrap">
 								<div class="head-user">{{userName1}}</div>
 								<div class="arrow-btm"></div>
 							</a>
-							<div class="login-out" v-show="userName1 != ''">退出登陆</div>
+							<div class="login-out" v-show="userName1 != ''">退出登录</div>
 							<div class="lang-op">
 								<div>CN</div>
 								<div class="arrow-btm"></div>
